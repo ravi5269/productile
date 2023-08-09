fccd7600575717d574bf6b200ecc71e909991004
ravi5269 <ravipatelkant@gmail.com>
2023-08-09 16:49:35 +0530

    env variable created

--- productile/settings.py
+++ productile/settings.py
@@ -85,9 +85,9 @@ WSGI_APPLICATION = "productile.wsgi.application"
 DATABASES = {
     "default": {
         "ENGINE": "django.db.backends.postgresql",
-        "NAME": "productile2",
-        "USER": "postgres",
-        "PASSWORD": "1234",
+        "NAME": env("DATABASE_NAME"),
+        "USER": env("DATABASE_USER"),
+        "PASSWORD":  env("DATABASE_PASS"),
         "HOST": "localhost",
         "PORT": "5432",
     }
