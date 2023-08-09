59d30a8b5274517559df408acef6adbe0728e62f
ravi5269 <ravipatelkant@gmail.com>
2023-08-09 16:52:51 +0530

    env variable setting

--- productile/settings.py
+++ productile/settings.py
@@ -14,6 +14,7 @@ from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
+# env variable
 import environ
 env = environ.Env()
 environ.Env.read_env()
@@ -22,6 +23,7 @@ from dotenv import load_dotenv
 import os
 
 load_dotenv()
+
 
 
 
